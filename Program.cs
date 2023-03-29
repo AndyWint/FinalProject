87d88420c23662a3955e9d8d4018322aff91f18c
[AndyWinter] <avake3d@yandex.ru>
2023-03-29 14:58:53 +0500

    Написал метод распечатки массива

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+void PrintArray(string[] array)
+{
+    int size = array.Length;
+    int i = 0;
+    while (i < size)
+    {
+        Console.Write(array[i]);
+        Console.Write(" ");
+        i++;
+    }
+}
+
+
+
+
+
+
+
+
